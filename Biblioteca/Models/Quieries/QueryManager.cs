67be7f15ffd79f2796fc8c266396e5f8a68b6301
LeurisCastillo <leurisa.delossantos@gmail.com>
2024-04-10 14:28:33 -0400

    second commit

---
+++ Biblioteca/Models/Quieries/QueryManager.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace Biblioteca.Models.Quieries
+{
+    public class QueryManager
+    {
+        private string _connectionString;
+
+        public QueryManager(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public DataTable ExecuteQuery(string query)
+        {
+            DataTable dataTable = new DataTable();
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                        {
+                            adapter.Fill(dataTable);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error executing query: {ex.Message}");
+            }
+
+            return dataTable;
+        }
+
+        public int ExecuteNonQuery(string query)
+        {
+            int rowsAffected = 0;
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        rowsAffected = command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error executing non-query: {ex.Message}");
+            }
+
+            return rowsAffected;
+        }
+    }
+
+    
+}
